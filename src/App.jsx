6da7f3f26e1bddebe7f6f1e43557874cba8aa5b2
olarotimi <ademolarotimi2000@gmail.com>
2023-10-12 02:20:31 +0100

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
+import "./App.css";
+import Companies from "./components/Companies/Companies";
+import Residencies from "./components/Residencies/Residencies";
+import Value from "./components/Value/Value";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <div className="white-gradient"></div>
+        <Header />
+        <Hero />
+      </div>
+
+      <Companies/>
+      <Residencies/>
+      <Value/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Companies/Companies.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./Companies.css"
+
+const Companies = () => {
+  return (
+    
+    <section className="c-wrapper">
+
+
+        <div className="paddings innerWidth flexCenter c-container">
+             
+             <img src='./prologis.png' alt='sponsor-logo'/>
+             <img src='./tower.png' alt='sponsor-logo'/>
+             <img src='./equinix.png' alt='sponsor-logo'/>
+             <img src='./realty.png' alt='sponsor-logo'/>
+
+        </div>
+    </section>
+  )
+}
+
+export default Companies
