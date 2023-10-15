16e70d346ed4424fa50b3d824f007e17cae1f377
olarotimi <ademolarotimi2000@gmail.com>
2023-10-15 16:30:01 +0100

    feat

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,25 +1,37 @@
-import React from "react";
-import "./Header.css"
-
-
+import React, { useState } from "react";
+import { BiMenuAltRight } from "react-icons/bi";
+import OutsideClickHandler from "react-outside-click-handler";
+import "./Header.css";
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const getMenuStyles = (menuOpen) => {
+    if (document.documentElement.clientWidth <= 800) {
+      return { right: !menuOpen && "-100%" };
+    }
+  };
   return (
     <section className="h-wrapper">
       <div className="flexCenter paddings innerWidth h-container">
         <img src="./logo.png" alt="logo" width={100} />
 
-        <div className="flexCenter h-menu">
-          <a href="">Residencies</a>
-          <a href="">Our Value</a>
-          <a href="">Contact Us</a>
-          <a href="">Get started</a>
+        <OutsideClickHandler
+          onOutsideClick={()=>setMenuOpen(false)}
+        >
+          <div className="flexCenter h-menu" style={getMenuStyles(menuOpen)}>
+            <a href="">Residencies</a>
+            <a href="">Our Value</a>
+            <a href="">Contact Us</a>
+            <a href="">Get started</a>
 
-          <button className="button">
-          <a href="">Contact</a>
+            <button className="button">
+              <a href="">Contact</a>
+            </button>
+          </div>
+        </OutsideClickHandler>
 
-          </button>
-          
+        <div className="menu-icon" onClick={() => setMenuOpen((prev) => !prev)}>
+          <BiMenuAltRight size={30} />
         </div>
       </div>
     </section>
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import {HiLocationMarker} from "react-icons/hi"
 import CountUp from "react-countup"
 import "./Hero.css";
+import {motion} from "framer-motion"
 
 const Hero = () => {
   return (
@@ -12,12 +13,19 @@ const Hero = () => {
         <div className="flexColStart hero-left">
           <div className="hero-title">
             <div className="orange-circle"></div>
-            <h1>
+            <motion.h1
+              initial={{y: "2rem", opacity: 0}}
+              animate={{y:0, opacity:1}}
+              transition={{
+                duration: 2,
+                type: "spring",
+              }}
+            >
               Discover <br />
               Most Suitable
               <br />
               Property
-            </h1>
+            </motion.h1>
 
             
           </div>
@@ -70,9 +78,16 @@ const Hero = () => {
 
         {/* Right side */}
         <div className=" flexCenter hero-right">
-          <div className="image-container">
+          <motion.div className="image-container"
+              initial={{x:"7rem", opacity: 0}}
+              animate={{x:"0", opacity: 1}}
+              transition={{
+                duration: 2,
+                type: "spring"
+              }}
+          >
             <img src="./hero-image.png" alt="" />
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>
